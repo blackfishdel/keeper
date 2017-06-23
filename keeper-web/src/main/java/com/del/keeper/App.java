bd37125bede121dc0bf63b0463fa0979137a54d8
del.xie <delxie@del.xie>
2017-06-23 17:21:51 +0800

    first commit

---
+++ keeper-web/src/main/java/com/del/keeper/App.java
@@ -0,0 +1,35 @@
+package com.del.keeper;
+
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * 
+ * @author delxie
+ *
+ */
+@PropertySource("classpath:configure.properties")
+@ImportResource("classpath:spring-base.xml")
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class,
+    DataSourceTransactionManagerAutoConfiguration.class, MybatisAutoConfiguration.class })
+public class App extends SpringBootServletInitializer {
+
+  /**
+   * web.xml初始化
+   */
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(App.class);
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+}
---
+++ keeper-web/target/classes/backup/initialize.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+
+#docker
+#	mysql
+#	web
+
+
+#启动数据库
